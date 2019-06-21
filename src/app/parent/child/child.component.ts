25137405f4985eac07b9f28a6e320fb2dd2ee4ed
friesam <frieda.musa@subccontrol.com>
2019-06-21 09:22:47 -0230

    viewchild has a bug

---
+++ src/app/parent/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ParentComponent } from '../../parent/parent.component';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss']
+})
+export class ChildComponent implements OnInit {
+@Input() childComponentMsg : string
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+viewchildFromChild : "I am the vine"
+}
---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, AfterViewInit, ViewChild } from '@angular/core';
+import { ChildComponent } from './child/child.component';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.scss']
+})
+export class ParentComponent implements OnInit, AfterViewInit {
+  @ViewChild(ChildComponent) viewChildMessage
+
+
+  parentMessage ="You are not sharing data using parent component"
+  viewchildFromParent: string
+  outputParentMessage ="Yes we did it!!!!!"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.viewchildFromParent = this.viewChildMessage.viewchildFromChild
+  }
+}
